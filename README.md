d3494e89b4d0b30830572ce2ece23e1eeff0e2af
eliagrady <admin@eliagrady.com>
2016-09-22 20:33:19 +0300

    add .editorconfig , add initial uuid generation

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# react-native-navigation-bootstrap
-##Bootstrap for a vanilla react-native-navigation based project
-
-Clone this repository to start a project based on react-native-navigation version 2.0.0-experimental.116
+# MovieHack
+##A movie suggestion app, powered by React Native
--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import * as reducers from './reducers';
 import {registerScreens} from './screens';
+import { AsyncStorage } from 'react-native';
+import { uuid } from 'uuid';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
@@ -20,7 +22,17 @@ registerScreens(store, Provider);
 
 export default class App {
   constructor() {
-
+    store.appDidLoad = false;
+    AsyncStorage.getItem('userKey').then((userId) => {
+      if(uuid) {
+        store.userKey = userId;
+        store.isUserNew = false;
+      }
+      else {
+        store.userKey = uuid.v4();
+        store.isUserNew = true;
+      }
+    })
   }
 
   startApp() {
