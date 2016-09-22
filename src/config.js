ae212b81c15b28e217444e40da2bcb4abb058c39
eliagrady <admin@eliagrady.com>
2016-09-23 02:00:56 +0300

    add trailer video url getter function

--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ export const TMDB_ENDPOINT = "https://api.themoviedb.org/3/";
 export const TMDB_DISCOVER = "discover/movie";
 export const TMDB_API_KEY = "?api_key=e05dd64e450edf78f2ad8d93fb0f1857";
 export const TMDB_POSTER_BASE = "https://image.tmdb.org/t/p/w370_and_h556_bestv2/";
+export const TMDB_TRAILER_FUNC = videoId => `http://api.themoviedb.org/3/movie/${videoId}/videos${TMDB_API_KEY}`;
