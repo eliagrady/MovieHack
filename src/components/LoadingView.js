8b36756553da6d7c82e2f43644637614ab5d67e2
eliagrady <admin@eliagrady.com>
2016-09-23 10:28:12 +0300

    loading screen tweak

--- src/components/LoadingView.js
+++ src/components/LoadingView.js
@@ -9,16 +9,24 @@ import {
   Platform
 } from 'react-native';
 
+import Spinner from 'react-native-loading-spinner-overlay';
+
 export default class LoadingView extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      visible: true
+    };
 
   }
 
   render() {
     return (
-      <View>
-        <Text>Loading Movies</Text>
+      <View style={{flex: 1,flexDirection: 'column',justifyContent: 'center',alignItems: 'center',}}>
+        <View style={{height: 50, width: 400}}>
+          <Spinner visible={this.state.visible} />
+          {/*<Text style={{alignSelf: 'center', justifyContent: 'center', fontSize: 26}}>Loading</Text>*/}
+        </View>
       </View>
     );
   }
